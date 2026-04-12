ee3e4a13d961c81ef2e6fe93f891a6b85f9598bd
Fedor Antonenko <cupofmilk80@gmail.com>
2026-04-12 14:31:40 +0300

    1) Десериализация снапшотов

--- telemetry/analyzer/src/main/java/ru/yandex/practicum/consumers/SnapshotProcessor.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/consumers/SnapshotProcessor.java
@@ -2,6 +2,9 @@ package ru.yandex.practicum.consumers;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.avro.io.Decoder;
+import org.apache.avro.io.DecoderFactory;
+import org.apache.avro.specific.SpecificDatumReader;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
@@ -13,11 +16,14 @@ import ru.yandex.practicum.entity.Snapshot;
 import ru.yandex.practicum.grpc.telemetry.event.DeviceActionProto;
 import ru.yandex.practicum.repository.ScenarioRepository;
 import ru.yandex.practicum.service.ScenarioExecutor;
+import ru.yandex.practicum.kafka.telemetry.event.SensorsSnapshotAvro;
+import ru.yandex.practicum.kafka.telemetry.event.SensorStateAvro;
 
 import java.time.Duration;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
+import java.util.HashMap;
 
 @Slf4j
 @Component
@@ -50,6 +56,8 @@ public class SnapshotProcessor {
                 }
                 consumer.commitSync();
             }
+        } catch (Exception e) {
+            log.error("Error in snapshot consumer loop", e);
         }
     }
 
@@ -80,10 +88,82 @@ public class SnapshotProcessor {
     }
 
     private Snapshot deserializeSnapshot(byte[] bytes) {
-        // TODO: десериализация Avro снапшота
-        return Snapshot.builder()
-                .hubId("test-hub")
-                .sensorValues(Map.of())
-                .build();
+        try {
+            SpecificDatumReader<SensorsSnapshotAvro> reader = new SpecificDatumReader<>(SensorsSnapshotAvro.class);
+            Decoder decoder = DecoderFactory.get().binaryDecoder(bytes, null);
+            SensorsSnapshotAvro avroSnapshot = reader.read(null, decoder);
+
+            Map<String, Object> sensorValues = new HashMap<>();
+
+            if (avroSnapshot.getSensorsState() != null) {
+                avroSnapshot.getSensorsState().forEach((sensorId, sensorState) -> {
+                    Object value = extractSensorValue(sensorState);
+                    if (value != null) {
+                        sensorValues.put((String) sensorId, value);
+                    }
+                });
+            }
+
+            return Snapshot.builder()
+                    .hubId(avroSnapshot.getHubId().toString())
+                    .sensorValues(sensorValues)
+                    .build();
+        } catch (Exception e) {
+            log.error("Failed to deserialize Avro snapshot", e);
+            throw new RuntimeException("Failed to deserialize snapshot", e);
+        }
+    }
+
+    private Object extractSensorValue(SensorStateAvro sensorState) {
+        if (sensorState.getData() == null) {
+            return null;
+        }
+
+        Object data = sensorState.getData();
+        String dataType = data.getClass().getSimpleName();
+
+        switch (dataType) {
+            case "MotionSensorAvro":
+                try {
+                    return data.getClass().getMethod("getMotion").invoke(data);
+                } catch (Exception e) {
+                    log.error("Failed to get motion value", e);
+                }
+                break;
+
+            case "TemperatureSensorAvro":
+                try {
+                    return data.getClass().getMethod("getTemperatureC").invoke(data);
+                } catch (Exception e) {
+                    log.error("Failed to get temperature value", e);
+                }
+                break;
+
+            case "LightSensorAvro":
+                try {
+                    return data.getClass().getMethod("getLuminosity").invoke(data);
+                } catch (Exception e) {
+                    log.error("Failed to get luminosity value", e);
+                }
+                break;
+
+            case "ClimateSensorAvro":
+                try {
+                    return data.getClass().getMethod("getTemperatureC").invoke(data);
+                } catch (Exception e) {
+                    log.error("Failed to get climate temperature value", e);
+                }
+                break;
+
+            case "SwitchSensorAvro":
+                try {
+                    return data.getClass().getMethod("getState").invoke(data);
+                } catch (Exception e) {
+                    log.error("Failed to get switch state value", e);
+                }
+                break;
+        }
+
+        return null;
     }
 }
